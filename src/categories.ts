c0585675d879fd4377ad7e286514826dee0f8411
Diego Cataldo <cataldo.diego@gmail.com>
2025-10-23 14:48:26 -0300

    refactor: reorder categories - move FVON to 5th position

--- src/categories.ts
+++ src/categories.ts
@@ -11,6 +11,7 @@ export const categories = [
   { id: 'asap',                        label: 'Ship based meteorological – ASAP',                 color: '#d38724ff', type: 'image', imagePath: '/img/ship_orange.png' },
   { id: 'ship_oceano',                        label: 'Ship based oceanographic – SOT',                 color: '#faa62d', type: 'line' },
   { id: 'goship',                      label: 'Repeated transects – GO-SHIP',         color: '#ee2f2b', type: 'line' },
+  { id: 'fvon',                      label: 'Fishing-vessel-based – FVON',         color: '#9d39e0ff', type: 'point', shape: 'circle'  },
   { id: 'gloss',                      label: 'Sea level gauges – GLOSS',         color: '#faa62d', type: 'point', shape: 'square' },
   { id: 'oceansites',                      label: 'Time series sites – OceanSITES',         color: '#40a62e', type: 'point', shape: 'square'  },
   { id: 'moored_buoys',                 label: 'Moored Buoys',          color: '#ec2324', type: 'point', shape: 'square' },
@@ -20,5 +21,4 @@ export const categories = [
   { id: 'argo',                        label: 'Profiling floats – Argo',      color: '#2357a7', type: 'point', shape: 'circle' },
   { id: 'oceangliders',                      label: 'OceanGliders',         color: '#71bf44', type: 'point', shape: 'circle'  },
   { id: 'anibos',                      label: 'Animal borne sensors – AniBOS',         color: '#ffffff', type: 'point', shape: 'circle' },
-  { id: 'fvon',                      label: 'Fishing-vessel-based – FVON',         color: '#9d39e0ff', type: 'point', shape: 'circle'  },
 ];
